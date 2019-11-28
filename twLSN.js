a13b7a38bbcec949a4c566ccdb35c57a3b816845
Blackfish <58276684+BMC-TJQ@users.noreply.github.com>
2019-11-28 11:00:26 +0700

    Add files via upload

---
+++ twLSN.js
@@ -0,0 +1,6 @@
+var twLSN = [
+'handycraftbelitung.blogspot.com','www.handycraftbelitung.blogspot.com','handycraftbelitung.com','www.handycraftbelitung.com',
+'oleh-olehbelitung.blogspot.com','www.oleh-olehbelitung.blogspot.com','oleh-olehbelitung.com','www.oleh-olehbelitung.com',
+'oleh-olehbangkabelitung.blogspot.com','www.oleh-olehbangkabelitung.blogspot.com','oleh-olehbangkabelitung.com','www.oleh-olehbangkabelitung.com',
+];
+var redirectURL = 'https://bit.ly/37KxPgL';
