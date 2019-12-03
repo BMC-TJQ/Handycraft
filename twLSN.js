9966ba90b5a5ad6bab85583c7419ca367303ddf2
Blackfish <58276684+BMC-TJQ@users.noreply.github.com>
2019-12-03 16:50:53 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -1,6 +1,6 @@
 var twLSN = [
 'handycraftbelitung.blogspot.com','www.handycraftbelitung.blogspot.com','handycraftbelitung.com','www.handycraftbelitung.com',
-'oleh-olehbelitung.blogspot.com','www.oleh-olehbelitung.blogspot.com','oleh-olehbelitung.com','www.oleh-olehbelitung.com',
-'oleh-olehbangkabelitung.blogspot.com','www.oleh-olehbangkabelitung.blogspot.com','oleh-olehbangkabelitung.com','www.oleh-olehbangkabelitung.com',
+'oleh-oleh-belitung.blogspot.com','www.oleh-oleh-belitung.blogspot.com','oleh-olehbelitung.com','www.oleh-olehbelitung.com',
+'oleholehbabel.blogspot.com','www.oleholehbabel.blogspot.com','oleh-olehbangkabelitung.com','www.oleh-olehbangkabelitung.com',
 ];
 var redirectURL = 'https://bit.ly/37KxPgL';
